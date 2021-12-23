2b837064c5b2b35a8ad9f9310be0aacc18d1f8be
BrainLucker <brainlucker@gmail.com>
2021-12-23 22:58:06 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9090;
+        int mileRatio = 20;
+        int miles = ticketPrice / mileRatio;
+        System.out.println("Спасибо за покупку! Вам начислится " + miles + " бонусные(-х) мили(-ь).");
+    }
+}
